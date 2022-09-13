0affe25bfe2f216386a1967ffa39110fe43814a8
JosueRenteria <92546462+JosueRenteria@users.noreply.github.com>
2022-09-12 21:57:47 -0500

    Ejercicio Ahorcado con Sockets-v1

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,6 @@
 from socket import *
 import sys # Terminar la ejecucion del codigo.
+import os # Utilizar funciones del cmd
 
 # Dirreccion Loopback y puerto de Envio.
 direccion_servidor = "127.0.0.1"
@@ -9,90 +10,197 @@ puerto_servidor = 9099
 socket_cliente = socket(AF_INET, SOCK_STREAM)
 socket_cliente.connect((direccion_servidor, puerto_servidor))
 
-
 #-------------------------------------------------------FUNCIONES-------------------------------------------------------
-
-# Funcion para el encabezado de inicio del juego.
+# Funcion para el encabezado de inicio del juego (estilos del programa).
 def Encabezado(vidas, palabra_secreta):
     # Opciones para las palabras.
-    print("----Bienvenido al Juego del Ahorcado----\n")
+    print("\t----------------------------------------")
+    print("\t----Bienvenido al Juego del Ahorcado----")
+    print("\t----------------------------------------\n")
     print(f"Te quedan {vidas} vidas.\n\n")
-    print("La palabra a Encotrar es:")
-    print(f"\t{palabra_secreta}")
+    print(f"La palabra a Encotrar es:\t{palabra_secreta}\n")
     
-# Funcion para ver si las palabras son iguales.
-def Comparacion(palabra_antugua, palabra_nueva, palabra_final, vidas):
-    # Opciones para las palabras.
-    if palabra_antugua == palabra_nueva:
-        palabra_antugua = palabra_nueva
+# Funcion para la comparacion de palabras y saber si son iguales.
+def Comparacion(palabra_antigua, palabra_nueva, vidas):
+    # Opciones cuando la palabra secreta es diferente a la palabra secreta resivida por el Servidor.
+    if palabra_antigua != palabra_nueva:
+        palabra_antigua = palabra_nueva
         print("La Letra que escogiste fue correcta.")
-    elif palabra_antugua == palabra_final:
-        vidas = 0
-        print(f"Filicidades Acabas de Encontrar la Palabra, que era: {palabra_final}")
+
+    # Cuando la letra es incorrecta y la palabra secreta es igual a la palabra secreta resivida por el Servidor.
     else:
-        vidas = vidas - 1
+        palabra_antigua = palabra_nueva
+        vidas = vidas - 1 # Restacion de vidas.
         print("La Letra que escogiste fue erronea.")
     
     # Retorno de la funcion
-    return palabra_antugua, vidas
+    return palabra_antigua, vidas
 
+# Encabezado cuando el jugador a Ganado.
+def Marco_ganador():
+    os.system('cls') # Borramos pantalla.
+    print("\t#############################")
+    print("\t#FELICIDADES ACABAS DE GANAR#")
+    print("\t#############################")
+    
+# Encabezado cuando el jugador a Perdido.
+def Marco_perdedor():
+    os.system('cls') # Borramos pantalla.
+    print("\t#########################")
+    print("\t#LO SENTIMOS HAS PERDIDO#")
+    print("\t#########################")
+        
+# Dibujo del Ahorcado dependiendo de los intentos.
+def ImprimirAhorcado(vidas):
+    if vidas == 0:
+        print("""
+                       ___
+                      |   |
+                     _O/  |
+                      |   |
+                     / \  |
+                    ______|
+        """)
+    elif vidas == 1:
+        print("""
+                       ___
+                      |   |
+                     _O/  |
+                      |   |
+                       \  |
+                    ______|
+        """)
+    elif vidas == 2:
+        print("""
+                       ___
+                      |   |
+                     _O/  |
+                      |   |
+                          |
+                    ______|
+        """)
+    elif vidas == 3:
+        print("""
+                       ___
+                      |   |
+                     _O/  |
+                          |
+                          |
+                    ______|
+        """)
+    elif vidas == 4:
+        print("""
+                       ___
+                      |   |
+                      O/  |
+                          |
+                          |
+                    ______|
+        """)
+    elif vidas == 5:
+        print("""
+                       ___
+                      |   |
+                      O   |
+                          |
+                          |
+                    ______|
+        """)
 #-------------------------------------------------------FUNCIONES-------------------------------------------------------
 
 # Mensaje de Bienvenida.
-print("BUENVENIDO AL CHAT CON EL SERVIDOR")
+print("\t\tBUENVENIDO AL CHAT CON EL SERVIDOR")
 
 while True:
-    # Entradas del programa.
+    # Menu inicial para ingresar la opcion de palabras a elegir.
     print("\nBienvenido al juego del Ahorcado escoge alguna de las siguientes opciones:\n")
-    print("   1_Palabra Corta.\n")
-    print("   2_Palabra Mediana.\n")
-    print("   3_Palabra Larga.\n")
-    print("   Cualquier otro numero para 'Salir'.\n")
-    opcion_palabra = input()
-    opcion_palabra_entero = int(opcion_palabra)
+    print("\t1_Palabra Corta.")
+    print("\t2_Palabra Mediana.")
+    print("\t3_Palabra Larga.")
+    print("\tCualquier otro numero para 'Salir'.\n")
+    opcion_palabra = input("Ingresa la opcion escogida: ") #Opcion elegida por el usuario.
 
-    # Por si la variable que se escogio es diferente a las opciones indicadas.
-    if opcion_palabra_entero <= 0 or opcion_palabra_entero >= 4:
-        # Eviamos mensaje.
-        socket_cliente.send(opcion_palabra.encode())
+    # Saber si la opcion que se eligio es un numero y si no lo es que vuelva a ingresar la opcion a elegir.
+    if (opcion_palabra.isnumeric()) == False: # .isnumeric sirve para saber 
+        print("\nNOTA: El dato que se ingreso no es un numero, porfavor ingresa un valor correcto.")
+        os.system('pause') # Pausamos la pantalla.
+        os.system('cls') # Borramos pantalla.
 
-        # Cerramos socket.
-        socket_cliente.close()
-        sys.exit()
-    
-    # Else para la condicion que si se optuvo correctamente.
+    # Si la opcion que se eligio es un numero entonces si entra en este proceso.
     else:
+        # Aqui convertimos la opcion de palabra en entero para una mejor implementacion.
+        opcion_palabra_entero = int(opcion_palabra)
 
-        # Enviar Mensaje (Opcion a elegir de palabras).
-        socket_cliente.send(opcion_palabra.encode())
+        # Por si la variable que se escogio es diferente a las opciones indicadas.
+        if opcion_palabra_entero <= 0 or opcion_palabra_entero >= 4:
+            # Eviamos mensaje.
+            socket_cliente.send(opcion_palabra.encode())
+
+            # Cerramos socket y mandamos el mensaje de Salida.
+            print("\nLa opcion que se eligio fue 'Salir', Desconectando del Servidor....")
+            os.system('pause') # Pausamos la pantalla.
+            socket_cliente.close()
+            sys.exit()
         
-        # Inicializacion de Vidas del Juego.
-        Vidas = 5
+        # Else para opciones elegidas por el Cliente a jugar..
+        else:
+            # Enviar la opcion de pañlabra a elegir.
+            socket_cliente.send(opcion_palabra.encode())
+            
+            # Inicializacion de las vidas del Juego.
+            Vidas = 5
 
-        # Resivimos mensaje del Servidor (la palabra a Buscar en Secreto).
-        palabra_encontrar_secreto = socket_cliente.recv(4096).decode()
-        print(f"Servidor-{palabra_encontrar_secreto}")
-#############################################################################################################################333
-        # Resivimos mensaje del Servidor (la palabra a Buscar).
-        palabra_final = palabra_encontrar_secreto = socket_cliente.recv(4096).decode()
+            # Resivimos mensaje del Servidor (la palabra a Buscar en Secreto y la original).
+            palabra_encontrar_secreto = socket_cliente.recv(4096).decode()
+            socket_cliente.sendall(b"Se necesita la palabra correcta.") # Enviamos un acuse para resivir la palabra final.
+            palabra_final = socket_cliente.recv(4096).decode()
 
-        while Vidas > 0:
-            # Encabezado para decir la palabra.
-            Encabezado(Vidas, palabra_encontrar_secreto)
+            # Impresiones de las palabras resividas (solo para verificar por parte del programador).
+            # print(f"La palabra secreta es: {palabra_encontrar_secreto}")
+            # print(f"La palabra es: {palabra_final}")
+            os.system('cls')
 
-            # Ingresar la letra a encontra.
-            print("Ingresa la letra a escoger")
-            letra = input()
+            # Ciclo para empezar a ingresar las letra a encontrar.
+            while Vidas > 0:
+                # Encabezado para decir la palabra.
+                Encabezado(Vidas, palabra_encontrar_secreto)
+                ImprimirAhorcado(Vidas)
 
-            # Enviamos la letra a encontrar.
-            socket_cliente.send(letra.encode())
+                # Ingresar la letra a encontrar.
+                letra = input("Ingresa la palabra a escoger: ").lower() # .lower() pone la letra en minuscula.
 
-            # Recivimos palabra nueva.
-            palabra_nueva = socket_cliente.recv(4096).decode()
+                #Condiciones para solo aceptar letras y no palabras.
+                if len(letra) != 1:
+                    print("\nPor favor solo Ingresa letras, no palabras ni mas de dos letras.")
+                    print("Si ya sabes la palabra, por favor ingresa letra por letra.\n")
+                    os.system('pause')
+                    os.system('cls')
 
-            # Aqui se compararan las palabras. 
-            palabra_encontrar_secreto, Vidas = Comparacion(palabra_encontrar_secreto, palabra_nueva, palabra_final, Vidas)
+                else:
+                    # Enviamos la letra a encontrar y resivimos del Servidor la nueva palabra secreta.
+                    socket_cliente.send(letra.encode())
+                    palabra_nueva = socket_cliente.recv(4096).decode()
 
+                    # Aqui se compararan las palabras y se decide si se encontro una letra. 
+                    palabra_encontrar_secreto, Vidas = Comparacion(palabra_encontrar_secreto, palabra_nueva, Vidas)
 
+                    # Si ya se tiene que salir porque el jugador perdio o porque gano.
+                    if palabra_encontrar_secreto == palabra_final:
+                        Vidas = 0
+                        Marco_ganador()
+                        print(f"\nFilicidades Acabas de Encontrar la Palabra, que era: {palabra_final}\n")
+                        os.system('pause') # Pausamos la pantalla.
+                    elif Vidas == 0:
+                        Marco_perdedor()
+                        ImprimirAhorcado(Vidas)
+                        print(f"\nLo sentimos te quedastes sin vidas, la palabra era: {palabra_final}\n")
+                        os.system('pause') # Pausamos la pantalla.
 
+                    # Borramos pantalla para cada juego.
+                    os.system('cls')
 
+                    #Aqui enviamos las vidas al Servidor y tambien termine su proceso.
+                    vida_palabra = str(Vidas)
+                    socket_cliente.send(vida_palabra.encode())
+                    mensaje = socket_cliente.recv(1024)
+                    # print(mensaje)
--- Servidor.py
+++ Servidor.py
@@ -14,22 +14,21 @@ socket_servidor = socket(AF_INET,SOCK_STREAM)
 socket_servidor.bind((direccion_servidor, puerto_servidor))
 socket_servidor.listen()    # Modo escucha del servidor.
 
-# Declaramos las listas de las palabras
-palabras_cortas = ['Hola', 'Adios', 'Dia']
-palabras_medianas = ['Paracaidas', 'Paracetamol']
-palabras_largas = ['Trinidad y Tobago']
+# Declaramos las listas de las palabras.
+palabras_cortas = ['hola', 'adios', 'dia', 'noche', 'casa', 'perros', 'gatos', 'estres', 'ahorrar', 'distancia']
+palabras_medianas = ['paracaidas', 'paracetamol', 'ovoviviparo', 'caleidoscopio', 'electrolisis', 'ferrocarril', 'remolachas', 'tecnologia', 'multicolinealidad', 'paralelepipedos']
+palabras_largas = ['trinidad y tobago', 'dias soleados', 'politecnico nacional', 'escuela superior de computo', 'maquinas inteligentes', 'cientificos tecnologicos', 'manejo de informacion', 'improvisacion de un humano', 'logica programable', 'inteligencia emocional']
 
 #-------------------------------------------------------FUNCIONES-------------------------------------------------------
-
 # Funcion para definir una palabra aleatoria.
 def crear_cadenas(opcion):
     # Opciones para las palabras.
     if opcion == 1:
-        palabra = random.choice(palabras_cortas)
+        palabra = random.choice(palabras_cortas) # Eleccion de palabras cortas.
     elif opcion == 2:
-        palabra = random.choice(palabras_medianas)
+        palabra = random.choice(palabras_medianas) # Eleccion de palabras medianas.
     elif opcion ==  3:
-        palabra.choice(palabras_largas)
+        palabra = random.choice(palabras_largas) # Eleccion de palabras largas.
     
     # Retorno de la funcion crear_cadena.
     secreto = '_'*len(palabra)
@@ -40,6 +39,7 @@ def remplazar_simbolo(palabra, secreto, simbolo):
     cantidad = palabra.count(simbolo)
     inicio = 0
 
+    # Bucle para cambiar las letras en la palabra secreta.
     for i in range(cantidad):
         pos = palabra.find(simbolo, inicio)
         secreto = secreto[:pos] + simbolo +secreto[pos+1:]
@@ -49,7 +49,7 @@ def remplazar_simbolo(palabra, secreto, simbolo):
 #-------------------------------------------------------FUNCIONES-------------------------------------------------------
 
 # Mensaje de Bienvenida.
-print("BUENVENIDO AL CHAT CON EL CLIENTE")
+print("\tBIENVENIDO AL CHAT CON EL CLIENTE")
 
 while True:
     # Establecemos la coneccion.
@@ -60,25 +60,45 @@ while True:
         # Recivimos la variable de la opcion que desea el usuario
         opcion_palabra = socket_conexion.recv(4096).decode()
         opcion_palabra_entero =int(opcion_palabra)
-        print(f"La opcion de palabras es: {opcion_palabra}")
+        print(f"La opcion de la palabras escogida por el Cliente {addr} es: {opcion_palabra}")
 
         # Por si la variable que se escogio es diferente a las opciones.
-        if opcion_palabra_entero <= 0 or opcion_palabra_entero > 4:
-            print("Opcion erronea.")
+        if opcion_palabra_entero <= 0 or opcion_palabra_entero >= 4:
+            # Salida del cliente (el cliente decidio salir del juego y rompemos los procesos).
+            print(f"El Cliente {addr} decidio salir del Juego.")
             break
         
-        #Si se escogio aqui se asigna la palabra que se enviara al Cliente.
+        #Si se escogio aqui se asignan las palabra que se enviaran al Cliente.
         palabra_encontrar, palabra_encontrar_secreto = crear_cadenas(opcion_palabra_entero)
+        palabra_encontrar_secreto = remplazar_simbolo(palabra_encontrar, palabra_encontrar_secreto, ' ')
 
-        # Enviar la palabra escondida.
+        # Envio de la palabra secreta y de la palabra original.
         socket_conexion.send(palabra_encontrar_secreto.encode())
-        print(f"La palabra a encontrar es: {palabra_encontrar}")
-        print(f"La palabra secreta a encontrar es: {palabra_encontrar_secreto}")
+        mensaje = socket_conexion.recv(1024) # Resivimos el mensaje de que el cliente resivio la palabra a encontrar.
+        print(mensaje)
+        socket_conexion.send(palabra_encontrar.encode())
+        print(f"\nLa palabra secreta a encontrar es: {palabra_encontrar_secreto}")
+        print(f"La palabra original a encontrar es: {palabra_encontrar}")
 
-        # Recivimos la letra a buscar en la palabra.
-        
-    
-    # Mensaje de Desconeccion del Cliente
+        # Inicializacion de las vidas que tendra el cliente.
+        Vidas = 5
+
+        # Ciclo para buscar las palabras que nos da el cliente, y las remplaza en la palabra secreta (para enviarlo al cliente).
+        while Vidas > 0:
+            # Resivimos la letra a buscar y se remplazan las letras.
+            letra = socket_conexion.recv(4096).decode()
+            palabra_nueva = remplazar_simbolo(palabra_encontrar, palabra_encontrar_secreto, letra)
+            palabra_encontrar_secreto = palabra_nueva
+
+            # Enviamos la nueva palabra resultante (al utilizar la funcion "remplazar_simbolo").
+            socket_conexion.send(palabra_nueva.encode())
+
+            # Optencion de las vidas que le quedan al cliente en su partida.
+            Vidas_letra = socket_conexion.recv(4096).decode()
+            Vidas = int(Vidas_letra)
+            socket_conexion.sendall(b"Vidas Resividas.") # Enviamos el mensaje de llegada de vidas al cliente.
+
+    # Mensaje de Desconeccion del Cliente cuando desea salir del juego.
     print("El cliente se esta desconectando", addr)
 
     # Cerramos conexion.
